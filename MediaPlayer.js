3631b92f2677b9188aa23e962ed592852d8210fa
kpir4 <kpir4@student.monash.edu>
2017-12-14 20:50:18 +1100

    Collection hide and show on navigation bar click

--- MediaPlayer.js
+++ MediaPlayer.js
@@ -1,5 +1,5 @@
 const wrapper = document.getElementById("wrapper");
-const collection_wrapper = document.createElement("div");
+var collection_state = {};
 
 function load_collection(collection_set){
 	var navigation = document.getElementById("navigation");
@@ -7,46 +7,68 @@ function load_collection(collection_set){
 	for(var i = 0; i < collection_set.length; i++){
 		var collection_item = document.createElement("li");
 		collection_item.textContent = collection_set[i];
-		collection_item.setAttribute("id", "collection_" + (i+1));
+		collection_item.setAttribute("id", "nav_item_" + (i+1));
 		navigation.appendChild(collection_item);
 	}
 }
 
 
-function display_albums(album_set, collection_name){
-	collection_wrapper.classList.add("music_collection");
-	wrapper.appendChild(collection_wrapper);
+function load_albums(album_set, collection_set){
+	for (var i = 0; i < 2; i++){
+		var collection_wrapper = document.createElement("div");
+		collection_wrapper.classList.add("music_collection");
+		collection_wrapper.setAttribute("id", "collection_" + (i+1));
+		wrapper.appendChild(collection_wrapper);
 
-	const heading = document.createElement("h1");
-	heading.textContent = collection_name;
-	collection_wrapper.appendChild(heading);
+		const heading = document.createElement("h1");
+		heading.textContent = collection_set[i];
+		collection_wrapper.appendChild(heading);
 
-	for(var i = 0; i < album_set.length; i++){
-		// Create album cover collection elements
-		var cover_wrapper = document.createElement("div");
-		var cover = document.createElement("img");
-		var title = document.createElement("span");
+		var collection = album_set["collection_" + (i+1)];
 
-		// Add context
-		cover.src = album_set[i].cover_img;
-		title.textContent = album_set[i].title;
+		// Initialise all music collection displays
+		collection_state["collection_" + (i+1)] = false;
 
-		// Add classes
-		cover_wrapper.classList.add("collection_cover");
+		for(var j = 0; j < collection.length; j++){
+			// Create album cover collection elements
+			var cover_wrapper = document.createElement("div");
+			var cover = document.createElement("img");
+			var title = document.createElement("span");
 
-		cover_wrapper.appendChild(cover);
-		cover_wrapper.appendChild(title);
-		collection_wrapper.appendChild(cover_wrapper);
+			// Add context
+			cover.src = collection[j].cover_img;
+			title.textContent = collection[j].title;
 
+			// Add classes
+			cover_wrapper.classList.add("collection_cover");
+
+			cover_wrapper.appendChild(cover);
+			cover_wrapper.appendChild(title);
+			collection_wrapper.appendChild(cover_wrapper);
+		}
 	}
 }
 
+// Changes displayed music collection
 const nav = document.getElementById("navigation");
 nav.addEventListener('click', function(e){
-	if(e.target.tagName == 'LI' && e.target.getAttribute("id") == "collection_2"){
+	if(e.target.tagName == 'LI'){
+		// Get collection number
+		const collection_num = e.target.getAttribute("id").slice(-1);
+
+		// Check if collection clicked is already displayed
+		collection_state["collection_" + collection_num] = true;
+		const collections = document.querySelectorAll(".music_collection");
+
+		// 
+		for(var i = 0; i < collections.length; i++){
+			if("collection_" + collection_num != collections[i].id){
+				collection_state["collection_" + collection_num] = false;
+				collections[i].style.display = "none";
+			}
+		}
+
+		const collection_wrapper = document.getElementById("collection_" + collection_num);
 		collection_wrapper.style.display = "block";
-	}
-	else{
-		collection_wrapper.style.display = "none";
 	}
 })
